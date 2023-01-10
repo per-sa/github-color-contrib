30641cf0aa3ce757fcc351bb0cec03e5d16e3613
matheusclmb <ccolombomatheus@gmail.com>
2023-01-10 02:28:28 +0000

    Feat: Added Functions to Buttons for user choice

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+      var activeTab = tabs[0];
+      var activeTabId = activeTab.id; // or do whatever you need
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var mode = document.getElementById('HalloweenMode');
+    // onClick's logic below:
+    mode.addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            var activeTab = tabs[0];
+            var activeTabId = activeTab.id; // or do whatever you need
+            chrome.scripting.insertCSS({
+                files: ["halloween-mode.css"],
+                target: { tabId: activeTabId },
+            });
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var mode = document.getElementById('WinterMode');
+    // onClick's logic below:
+    mode.addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            var activeTab = tabs[0];
+            var activeTabId = activeTab.id; // or do whatever you need
+            chrome.scripting.insertCSS({
+                files: ["winter-mode.css"],
+                target: { tabId: activeTabId },
+            });
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var mode = document.getElementById('disableMode');
+    // onClick's logic below:
+    mode.addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            var activeTab = tabs[0];
+            var activeTabId = activeTab.id; // or do whatever you need
+
+            chrome.scripting.removeCSS({
+                files: ["halloween-mode.css"],
+                target: { tabId: activeTabId },
+            });
+
+            chrome.scripting.removeCSS({
+                files: ["winter-mode.css"],
+                target: { tabId: activeTabId },
+            });
+
+        });
+    });
+});
+    
